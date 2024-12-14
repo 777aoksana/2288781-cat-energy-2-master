d4127896f8d357b807edfc91d3b28dd4133b42c1
AreOks <aoksana@yandex.ru>
2024-12-14 09:29:49 +0300

    Удаляет скрипты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,37 +2,6 @@
 
 // меню
 
-const navMain = document.querySelector('.main-nav__wrapper');
-const buttonToggle = document.querySelector('.main-nav__toggle');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav__wrapper--nojs');
-navMain.classList.remove('main-nav__wrapper--open');
-navMain.classList.add('main-nav__wrapper--close');
-buttonToggle.classList.add('main-nav__toggle--open');
-buttonToggle.classList.remove('main-nav__toggle--nojs');
-
-
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav__wrapper--close')) {
-    navMain.classList.remove('main-nav__wrapper--close');
-    navMain.classList.add('main-nav__wrapper--open');
-  } else {
-    navMain.classList.add('main-nav__wrapper--close');
-    navMain.classList.remove('main-nav__wrapper--open');
-  }
-});
-
-navToggle.addEventListener('click', () => {
-  if (navToggle.classList.contains('main-nav__toggle--open')) {
-    navToggle.classList.remove('main-nav__toggle--open');
-    navToggle.classList.add('main-nav__toggle--close');
-  } else {
-    navToggle.classList.add('main-nav__toggle--open');
-    navToggle.classList.remove('main-nav__toggle--close');
-  }
-});
-
 
 // ползунок
 
