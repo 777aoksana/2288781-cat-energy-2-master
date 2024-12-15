293b412b092a4efc41aac39e145fc57f5337762c
AreOks <aoksana@yandex.ru>
2024-12-15 18:54:18 +0300

    Верстает декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,7 +4,7 @@
 // Кнопка "Гамбургер"
 const navMain = document.querySelector('.main-nav');
 const body = document.querySelector('body');
-const navToggle = document.querySelector('.header__toggle');
+const navToggle = document.querySelector('.page-header__toggle');
 
 body.classList.remove('nojs');
 
@@ -13,13 +13,13 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
 
-    navToggle.classList.remove('header__toggle--closed');
-    navToggle.classList.add('header__toggle--opened');
+    navToggle.classList.remove('page-header__toggle--closed');
+    navToggle.classList.add('page-header__toggle--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
 
-    navToggle.classList.add('header__toggle--closed');
-    navToggle.classList.remove('header__toggle--opened');
+    navToggle.classList.add('page-header__toggle--closed');
+    navToggle.classList.remove('page-header__toggle--opened');
   }
 });
