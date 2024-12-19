8c5a0be1d21eb3599ecdfb296723928bf94a4bd0
AreOks <aoksana@yandex.ru>
2024-12-19 22:50:28 +0300

    Исправляет раздел hero в десктопной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,17 +3,20 @@
 
 // Кнопка "Гамбургер"
 const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+const navToggle = document.querySelector('.page-header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+    navMain.classList.add('main-nav--open');
+    navToggle.classList.remove('page-header__toggle--close');
+    navToggle.classList.add('page-header__toggle--open');
   } else {
     navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.remove('main-nav--open');
+    navToggle.classList.add('page-header__toggle--close');
+    navToggle.classList.remove('page-header__toggle-nav--open');
   }
-}
-);
+});
