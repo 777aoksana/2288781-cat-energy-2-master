4c6499fc904d0adde402861f996aaae54296fa18
AreOks <aoksana@yandex.ru>
2024-12-19 07:00:43 +0300

    Приводит в порядок шапку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,19 +2,18 @@
 
 
 // Кнопка "Гамбургер"
-(function () {
-  const navMain = document.querySelector('.main-nav');
-  const navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-  navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--nojs');
 
-  navToggle.addEventListener('click', () => {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-    }
-  });
-})();
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+}
+);
